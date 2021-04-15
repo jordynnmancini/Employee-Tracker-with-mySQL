489d967827bea010c019c3497a003a16a4419c82
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-04-15 09:12:04 -0400

    set up mySQL connection/data tables/etc

---
+++ employeeManagement.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer'); 
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    
+    password: '',
+    database: 'employeesDB',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    initialize(); 
+})
+
+const initialize = () => {
+    // inquirer prompts 
+}
